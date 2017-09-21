2fddd41117fe078b17c6c883f68da75720105cc1
Scott Schaefer <scottschaef@gmail.com>
2017-09-20 23:13:47 -0400

    Improve discover component styling

--- components/Image.js
+++ components/Image.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Text, View, Button, FlatList, TouchableOpacity, Image, ImageBackground } from 'react-native';
-import YelpApi from 'v3-yelp-api';
-import categories from '../data/categories.js';
-import store from '../store/locationStore';
 
 export default class Discover extends Component {
 
@@ -31,6 +28,7 @@ export default class Discover extends Component {
 
   //Yelp Fetch that goes through Python
   fetchYelpData (title) {
+    // console.log('about to fetch');
     fetch('http://localhost:3000/api/yelp', {
           method: 'POST',
           headers: {'Content-Type': 'application/json'},
@@ -42,11 +40,10 @@ export default class Discover extends Component {
           })
     })
     .then((response) => {
-      //console.log(response);
+      console.log(response);
       var filtered = JSON.parse(response._bodyInit);
-      console.log('parsed filter: ', filtered);
       filtered = JSON.parse(filtered[0]);
-      //console.log('retreived data: ', filtered);
+      console.log('retreived data: ', filtered);
       this.props.navigation.navigate('CategoryView', {
         data: filtered,
         category: title
@@ -55,6 +52,30 @@ export default class Discover extends Component {
     .catch(err => console.log)
   }
 
+  // fetchYelpData (title) {
+  //   const credentials = {
+  //     appId: YelpConfig.appId,
+  //     appSecret: YelpConfig.appSecret
+  //   }
+  //   const yelp = new YelpApi(credentials);
+  //   var lat = this.state.latitude;
+  //   var lng = this.state.longitude;
+  //   var latlng = String(lat) + ',' + String(lng);
+  //   let params = {
+  //     term: title,
+  //     location: latlng,
+  //     limit: '30',
+  //   };
+  //   yelp.search(params)
+  //     .then((data) => {
+  //       this.props.navigation.navigate('CategoryView', {
+  //         data: data.businesses,
+  //         category: title })
+  //       //console.log('State: ',this.state);
+  //     })
+  //     .catch((err) => console.log(err));
+  // }
+
   render () {
     return (
       <View style={styles.container}>
@@ -62,18 +83,21 @@ export default class Discover extends Component {
           data={this.state.categories}
           keyExtractor={(category, index) => index }
           renderItem={({ item }) =>
-            <TouchableOpacity
-              onPress={ () =>  this.fetchYelpData(item.title) }>
-              <ImageBackground
-                style={styles.image}
-                source={{uri: item.image_url}}
-              >
-                <View style={styles.textContainer}>
-                    <Text style={styles.text}>{item.title}</Text>
+            <View style={styles.container}>
+              <View style={styles.image}>
+                <Image
+                  source={{uri: item.image_url}}
+                />
+              </View>
+              <View style={{
+                 height:100, width:400}}>
+                <TouchableOpacity
+                  onPress={ () =>  this.fetchYelpData(item.title) }>
+                  <Text style={styles.text}>{item.title}</Text>
 
-                </View>
-              </ImageBackground>
-            </TouchableOpacity>
+                </TouchableOpacity>
+              </View>
+            </View>
           }
         />
       </View>
@@ -85,25 +109,21 @@ const styles = StyleSheet.create({
   toolbarTab: {
     fontSize: 20
   },
-  image: {
+  container: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
+  },
+  image: {
+    flex: 1,
     borderBottomWidth: 1,
     borderColor: 'gray',
     height: 100,
   },
-  textContainer: {
-    backgroundColor: 'rgba(0,0,0,.4)',
-    height:100,
-    width: 400,
-    padding: 10
-  },
   text: {
     backgroundColor: 'rgba(0,0,0,0)',
     fontWeight: 'bold',
-    color: '#F7F7F7',
-    fontFamily: 'Avenir Light',
-    fontSize: 18
+    color: 'black',
+    opacity: 1
   },
 })
